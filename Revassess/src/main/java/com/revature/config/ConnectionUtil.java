a63c659d552b49f1c537d2b08237cefe104e3dac
github username <meliajm@gmail.com>
2020-08-26 15:11:51 -0500

    10 points tier3

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,21 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://javafs200803.c3dwgeuwlgd5.us-east-2.rds.amazonaws.com:5432/";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "postgres";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "password";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
-	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+//	CREATE SEQUENCE customers_seq
+//	 START WITH     1000
+//	 INCREMENT BY   1
+//	 NOCACHE
+//	 NOCYCLE; CREATE SEQUENCE a START WITH 6 INCREMENT BY 3
+	// name of the created sequence in tier 3     
+//	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "CREATE SEQUENCE START WITH 6 INCREMENT BY 3";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){
