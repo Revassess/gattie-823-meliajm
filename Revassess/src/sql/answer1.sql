a101f595c6012ffe8856f295493d3323e27f37f4
github username <meliajm@gmail.com>
2020-08-26 14:29:21 -0500

    30 points, first two sql answers

---
+++ Revassess/src/sql/answer1.sql
@@ -0,0 +1,1 @@
+SELECT * FROM app_user au join user_role ur on au.role_id = ur.role_id where ur.role_id = 3;
---
+++ Revassess/src/sql/answer2.sql
@@ -0,0 +1,3 @@
+insert into app_user(username, password, first_name, last_name, role_id)
+	values ('peanutbutter', 'elcapitan', 'miller',  'captain', 4)
+;
