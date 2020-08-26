b5af5e9c47243caace7062612ac058a0222f87c4
github username <meliajm@gmail.com>
2020-08-26 13:55:05 -0500

    70 points

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,21 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		
+		int[]digits= Long.toString(num).chars().map(c -> c-'0').toArray();
+		int sum = 0;
+		for( int i : digits) {
+//		    System.out.println(i);
+		}
+		
+		for( int i : digits) {
+		    sum += Math.pow(i, digits.length);
+		}
+	    System.out.println(sum);
+
+		if (sum == num) {
+			return true;
+		}
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,19 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+
+		int sum = 0;
+		if (arr.length>0 && arr!=null) {
+				
+			for (int i=0; i<arr.length; i++) {
+	//			System.out.println(arr[i]);
+	
+				sum += arr[i];
+			}
+			System.out.println(sum);
+			return sum;
+		} else {
+			return 0;
+		}
 	}
 }
