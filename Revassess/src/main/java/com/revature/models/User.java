9d24f5750eb1badd917a07ddb840809bf6f9a8d7
github username <meliajm@gmail.com>
2020-08-26 13:17:12 -0500

    user model

---
+++ Revassess/src/main/java/com/revature/models/User.java
@@ -0,0 +1,60 @@
+package com.revature.models;
+
+public class User {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String role;
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+//		result = prime * result + id;
+//		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+//		result = prime * result + ((password == null) ? 0 : password.hashCode());
+//		result = prime * result + ((role == null) ? 0 : role.hashCode());
+//		return result;
+//	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+	
+	
+
+
+}
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,16 @@
 package com.revature.tier1;
 
 public class User {
+		
+		private static final long serialVersionUID = 1L;
+		
+		private int id;
+		private String firstName;
+		private String lastName;
+		private String password;
+		private String role;
+
+
+
 }
+
